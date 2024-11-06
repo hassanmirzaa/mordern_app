526f89f175e4d81d19dde0974e7662203955eee3
Hassan Mirza <141772015+hassanmirzaa@users.noreply.github.com>
2024-11-07 02:05:36 +0500

    Ui

---
+++ lib/PageView/page2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatefulWidget {
+  const Page2({super.key});
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            color: Colors.deepPurple[400],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:the3d_objects/PageView/page1.dart';
+import 'package:the3d_objects/PageView/page2.dart';
+import 'package:the3d_objects/PageView/page3.dart';
+import 'package:the3d_objects/PageView/page4.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,10 +13,47 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[300],
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 500,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: PageView(
+                  controller: _controller,
+                  children: const [
+                    Page1(),
+                    Page2(),
+                    Page3(),
+                    Page4()
+                    
+                  ],
+                ),
+              ),
+            ),
+
+            SmoothPageIndicator(
+              controller: _controller,
+               count: 4,
+               effect: JumpingDotEffect(
+                dotColor: Colors.deepPurple.shade100,
+                activeDotColor: Colors.deepPurple,
+                spacing: 16,
+              //  verticalOffset: 20,
+                jumpScale: 2
+               ),
+               )
+
+          ],
+        ),
+      ),
     );
   }
 }
